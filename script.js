4155b44c1180da829c1504b82a3ceab3d35f35de
jhogan166 <84137079+jhogan166@users.noreply.github.com>
2021-05-13 08:52:13 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".yes-button").click(function() {
+    $(".opening-lines").hide();
+    $(".yes-route").show();
+});
+$(".no-button").click(function() {
+    $(".opening-lines").hide();
+    $(".no-route").show();
+});
+$(".fight").click(function() {
+    $(".candy").toggle();
+});
+$(".save him").click(function() {
+    $(".dennis").toggle();
+});
